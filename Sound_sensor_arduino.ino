dacc5ca8adad8b60a32dd44f5aae9b9ca959a121
Paweł Koźmiński <61986866+PawelKozminskiPoznan@users.noreply.github.com>
2020-05-12 16:34:08 +0200

    Add files via upload

---
+++ Sound_sensor_arduino.ino
@@ -0,0 +1,106 @@
+/*
+ * Rui Santos 
+ * Complete Project Details http://randomnerdtutorials.com
+*/
+#include <ArduinoJson.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHT11_PIN 12
+#define ledPin 13
+#define sensorPin A0
+
+DHT dht(DHT11_PIN,DHT11);
+float temp,humi,prev_temp,prev_humi;
+int ad_value = 0;
+int previous = 0;
+
+bool clap = false;
+bool threshold = false;
+
+unsigned long clap_detector = 0;
+unsigned long previousMillis = 0;
+unsigned long TMillis = 0;
+const long interval = 5;
+const long Tinterval = 10000;
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Serial.begin (115200);
+  dht.begin(); 
+}
+  
+void loop (){
+  unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      ad_value=analogRead(sensorPin);
+        if(!threshold and ((ad_value - previous) >350) ){
+		      threshold = true;
+          clap_detector = millis();
+		      if(!clap) {
+			      char json[] = "{\"type\":\"on\",\"value\":\"ON\"}"; 
+            Serial.println(json);
+			      digitalWrite(ledPin,HIGH);
+			      clap = true;
+		      }
+		      else{
+			      char json[] = "{\"type\":\"off\",\"value\":\"OFF\"}";
+            digitalWrite(ledPin,LOW);
+            Serial.println(json);
+			      clap = false;
+		      }
+        }
+      previous = ad_value;
+      if(threshold and ((currentMillis - clap_detector) >= 1000)) threshold = false;
+    }
+
+    if (currentMillis - TMillis >= Tinterval) {
+      TMillis = currentMillis;
+      temp = dht.readTemperature();
+      humi = dht.readHumidity();
+      if(prev_temp != temp){
+        String jsonString2 = "{\"type\":\"temp\",\"value\":\"";
+        jsonString2 += temp;
+        jsonString2 +="\"}"; 
+        Serial.println(jsonString2);
+      }
+      if(prev_humi != humi){
+        String jsonString3 = "{\"type\":\"humi\",\"value\":\"";
+        jsonString3 += humi;
+        jsonString3 +="\"}"; 
+        Serial.println(jsonString3);
+      }
+      prev_temp = temp;
+      prev_humi = humi;
+    }
+
+    if (Serial.available()) {
+      char type = (char)Serial.read();
+      //StaticJsonBuffer<200> jb;
+      //JsonObject& root = jb.parseObject(h);
+      //char type = root["type"];
+      if (type == 'c'){
+          char json1[] = "{\"type\":\"on\",\"value\":\"ON\"}";
+          digitalWrite(ledPin,HIGH);
+          Serial.println(json1);
+      }
+      else if (type == 'b'){
+          char json[] = "{\"type\":\"off\",\"value\":\"OFF\"}";
+          digitalWrite(ledPin,LOW);
+          Serial.println(json);
+      }
+      else if (type == 'z'){
+          String jsonString1 = "{\"type\":\"temp\",\"value\":\"";
+          jsonString1 += temp;
+          jsonString1 +="\"}"; 
+          Serial.println(jsonString1);
+      }
+      else if (type == 'x'){
+          String jsonString = "{\"type\":\"humi\",\"value\":\"";
+          jsonString += humi;
+          jsonString +="\"}";
+          Serial.println(jsonString);
+      }
+    }
+}
---
+++ Sound_to_net.ino
@@ -0,0 +1,72 @@
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
+#include <Hash.h>
+
+const char* ssid = "Embeded113";
+const char* password = "Embeded113";
+int previous = 0;
+unsigned long previousMillis = 0;
+const long interval = 10;
+
+String WebPage = "<!DOCTYPE html><html><style>input[type=\"button\"]{width: 20%;height: 3.6vh;}.rxd{height: 90vh;}textarea {width: 100%;height: 20%;font-size: 18px;text-align-last: center;}</style><script>var Socket;function start() \n{Socket = new WebSocket('ws://' + window.location.hostname + ':81/');Socket.onmessage = function(evt) \n{var data = evt.data.toString();\n data = data.substring(0, (data.length-1)); \n var msg = JSON.parse(data); \nswitch(msg.type) {case \"on\":document.getElementById(\"rxConsole\").value = msg.value; break;case \"off\": document.getElementById(\"rxConsole\").value = msg.value; break; case \"temp\": document.getElementById(\"temp\").value = msg.value; break; case \"humi\": document.getElementById(\"humi\").value = msg.value; break; }}} function temperature() { Socket.send('z');} function humidity() {Socket.send('x');}function on() {Socket.send('c');} function off() {Socket.send('b');}</script><body onload=\"javascript:start();\"><br><div class=\"rxd\"><font size=\"6\"><center>Temperatura</center></font><div><textarea id=\"temp\" readonly></textarea><br><input type=\"button\" onclick=\"temperature();\" value=\"Odczyt temperatury\" ></div><br><font size=\"6\"><center>Wilgotnosc</center></font><div><textarea id=\"humi\" readonly></textarea><br><input type=\"button\" onclick=\"humidity();\" value=\"Odczyt wilgotnosci\" ></div><br><font size=\"6\"><center>Oswietlenie</center></font><div><textarea id=\"rxConsole\" readonly></textarea><br><input type=\"button\" onclick=\"on();\" value=\"Zapal\" ><input type=\"button\" onclick=\"off();\" value=\"Zgas\" ></div></div></body></html>";
+//document.getElementById(\"txbuff\").value
+WebSocketsServer webSocket = WebSocketsServer(81);
+ESP8266WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+   }
+    
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    server.on("/", [](){
+    server.send(200, "text/html", WebPage);
+    });
+    
+    server.begin();
+    
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+}
+
+void loop() {
+    webSocket.loop();
+    server.handleClient();
+    //unsigned long currentMillis = millis();
+    //if (currentMillis - previousMillis >= interval) {
+        //previousMillis = currentMillis;
+        if (Serial.available()) {
+          String str = Serial.readStringUntil('\n');
+          int str_len = str.length(); 
+          char c [str_len];
+          str.toCharArray(c, str_len);
+          Serial.println(c);
+          webSocket.broadcastTXT(c, sizeof(c));
+        }
+    //}
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length){
+   for(int i = 0; i < length; i++) Serial.print((char) payload[i]);
+   Serial.println();
+}
+
+  /*char c[] = {(char)ad_value};
+  webSocket.broadcastTXT(c, sizeof(c));
+  bool val = 0;
+  val=digitalRead(sensorPinDIG);
+  char c2[] = {(char)val};
+  webSocket.broadcastTXT(c2, sizeof(c2));*/
